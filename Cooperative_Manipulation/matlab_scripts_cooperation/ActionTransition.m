c2a7e2880a65a641c0bde645f1ee9a7a7b92a74a
Davide Tonelli <davtonell@gmail.com>
2025-02-01 18:31:27 +0100

    added new ActionTransition to second part

--- Cooperative_Manipulation/matlab_scripts_cooperation/ActionTransition.m
+++ Cooperative_Manipulation/matlab_scripts_cooperation/ActionTransition.m
@@ -1,12 +1,28 @@
-function [A] = ActionTransition(taskname, previous, current, time)
-if (ismember(taskname, previous) && ismember(taskname, current))
-    A = 1;
-elseif (ismember(taskname, previous) == 0 && ismember(taskname, current))
-    A = IncreasingBellShapedFunction(0, 1, 0, 1, time);
-elseif (ismember(taskname, previous) && ismember(taskname, current) == 0)
-    A = DecreasingBellShapedFunction(0, 1, 0, 1, time);
+function [A] = ActionTransition(taskname, previous, current, time, constraint)
+% This function produce the sigmoid function to activate or deactivate tasks
+% according to the current and previous phase of the mission.
+% Takes as input the current task name ("T","RC","JL","MA"), the previous
+% task, the current one and the mission time. If doubts, see the Matlab doc of
+% ismember()
+if constraint
+    if (ismember(taskname, previous) && ismember(taskname, current))
+        A = 1;
+    elseif (ismember(taskname, previous) == 0 && ismember(taskname, current))
+        A = 1;
+    elseif (ismember(taskname, previous) && ismember(taskname, current) == 0)
+        A = 0;
+    else
+        A = 0;
+    end
 else
-    A = 0;
-
+    if (ismember(taskname, previous) && ismember(taskname, current))
+        A = 1;
+    elseif (ismember(taskname, previous) == 0 && ismember(taskname, current))
+        A = IncreasingBellShapedFunction(0, 1, 0, 1, time);
+    elseif (ismember(taskname, previous) && ismember(taskname, current) == 0)
+        A = DecreasingBellShapedFunction(0, 1, 0, 1, time);
+    else
+        A = 0;
+    end
 end
 
--- Cooperative_Manipulation/matlab_scripts_cooperation/ComputeActivationFunctions.m
+++ Cooperative_Manipulation/matlab_scripts_cooperation/ComputeActivationFunctions.m
@@ -3,7 +3,7 @@ function [pandaArm] = ComputeActivationFunctions(pandaArm,mission)
 switch mission.phase
     case 1
         % Minimum Altitude Task ( > 0.15m, 0.05m delta )
-        pandaArm.A.minimumAltitude = DecreasingBellShapedFunction(0.15, 0.2, 0, 1, pandaArm.wTt(3, 4)) * ActionTransition("MA", mission.actions.(mission.prev_action).tasks, mission.actions.(mission.current_action).tasks, mission.phase_time);
+        pandaArm.A.minimumAltitude = DecreasingBellShapedFunction(0.15, 0.2, 0, 1, pandaArm.wTt(3, 4)) * ActionTransition("MA", mission.actions.(mission.prev_action).tasks, mission.actions.(mission.current_action).tasks, mission.phase_time, false);
 
         % Joint Limits Task
         % Activation function: two combined sigmoids, which are at their maximum
@@ -15,26 +15,26 @@ switch mission.phase
             pandaArm.A.jointLimits(index,index) = DecreasingBellShapedFunction(jl(1), jl(1) + pandaArm.delta_perc * (jl(2) - jl(1)), 0, 1, pandaArm.q(index)) + IncreasingBellShapedFunction(jl(2) - pandaArm.delta_perc * (jl(2) - jl(1)), jl(2), 0, 1, pandaArm.q(index));
             index = index + 1;
         end
-        pandaArm.A.jointLimits = pandaArm.A.jointLimits * ActionTransition("JL", mission.actions.(mission.prev_action).tasks, mission.actions.(mission.current_action).tasks, mission.phase_time);
+        pandaArm.A.jointLimits = pandaArm.A.jointLimits * ActionTransition("JL", mission.actions.(mission.prev_action).tasks, mission.actions.(mission.current_action).tasks, mission.phase_time, false);
         
-        pandaArm.A.moveTool = eye(6) * ActionTransition("T", mission.actions.(mission.prev_action).tasks, mission.actions.(mission.current_action).tasks, mission.phase_time);
+        pandaArm.A.moveTool = eye(6) * ActionTransition("T", mission.actions.(mission.prev_action).tasks, mission.actions.(mission.current_action).tasks, mission.phase_time, false);
         
     case 2 % Same as before
-        pandaArm.A.minimumAltitude = DecreasingBellShapedFunction(0.15, 0.2, 0, 1, pandaArm.wTt(3, 4)) * ActionTransition("MA", mission.actions.(mission.prev_action).tasks, mission.actions.(mission.current_action).tasks, mission.phase_time);
+        pandaArm.A.minimumAltitude = DecreasingBellShapedFunction(0.15, 0.2, 0, 1, pandaArm.wTt(3, 4)) * ActionTransition("MA", mission.actions.(mission.prev_action).tasks, mission.actions.(mission.current_action).tasks, mission.phase_time, false);
         index = 1;
         for jl = [pandaArm.jlmin; pandaArm.jlmax]
             pandaArm.A.jointLimits(index,index) = DecreasingBellShapedFunction(jl(1), jl(1) + pandaArm.delta_perc * (jl(2) - jl(1)), 0, 1, pandaArm.q(index)) + IncreasingBellShapedFunction(jl(2) - pandaArm.delta_perc * (jl(2) - jl(1)), jl(2), 0, 1, pandaArm.q(index));
             index = index + 1;
         end
-        pandaArm.A.jointLimits = pandaArm.A.jointLimits * ActionTransition("JL", mission.actions.(mission.prev_action).tasks, mission.actions.(mission.current_action).tasks, mission.phase_time);
+        pandaArm.A.jointLimits = pandaArm.A.jointLimits * ActionTransition("JL", mission.actions.(mission.prev_action).tasks, mission.actions.(mission.current_action).tasks, mission.phase_time, false);
         
-        pandaArm.A.moveTool = eye(6) * ActionTransition("T", mission.actions.(mission.prev_action).tasks, mission.actions.(mission.current_action).tasks, mission.phase_time);
+        pandaArm.A.moveTool = eye(6) * ActionTransition("T", mission.actions.(mission.prev_action).tasks, mission.actions.(mission.current_action).tasks, mission.phase_time, false);
 
     case 3
-        pandaArm.A.minimumAltitude = DecreasingBellShapedFunction(0.15, 0.2, 0, 1, pandaArm.wTt(3, 4)) * ActionTransition("MA", mission.actions.(mission.prev_action).tasks, mission.actions.(mission.current_action).tasks, mission.phase_time);
-        pandaArm.A.jointLimits = pandaArm.A.jointLimits * ActionTransition("JL", mission.actions.(mission.prev_action).tasks, mission.actions.(mission.current_action).tasks, mission.phase_time);
-        pandaArm.A.moveTool = eye(6) * ActionTransition("T", mission.actions.(mission.prev_action).tasks, mission.actions.(mission.current_action).tasks, mission.phase_time);
-        pandaArm.A.stopAll = eye(7) * ActionTransition("NM", mission.actions.(mission.prev_action).tasks, mission.actions.(mission.current_action).tasks, mission.phase_time);
+        pandaArm.A.minimumAltitude = DecreasingBellShapedFunction(0.15, 0.2, 0, 1, pandaArm.wTt(3, 4)) * ActionTransition("MA", mission.actions.(mission.prev_action).tasks, mission.actions.(mission.current_action).tasks, mission.phase_time, false);
+        pandaArm.A.jointLimits = pandaArm.A.jointLimits * ActionTransition("JL", mission.actions.(mission.prev_action).tasks, mission.actions.(mission.current_action).tasks, mission.phase_time, false);
+        pandaArm.A.moveTool = eye(6) * ActionTransition("T", mission.actions.(mission.prev_action).tasks, mission.actions.(mission.current_action).tasks, mission.phase_time, false);
+        pandaArm.A.stopAll = eye(7) * ActionTransition("NM", mission.actions.(mission.prev_action).tasks, mission.actions.(mission.current_action).tasks, mission.phase_time, false);
 end
 
 end
